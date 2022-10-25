f814d527a752afb7263ed42e2f9eecfbb3fb8792
Anatoliy <bugrov.anatoliy@yandex.ru>
2022-10-25 22:43:50 +1000

    some commit15

--- src/FinanceApplication.java
+++ src/FinanceApplication.java
@@ -6,7 +6,7 @@ public class FinanceApplication {
     double balance;
     HashMap<String, ArrayList<Double>> expenses;
     Converter converter;
-    final Scanner scanner;
+    final Scanner scanner; //test
 
     public FinanceApplication(double rubles, Scanner scanner) {
         balance = rubles;
